e9213b99c8e4823cc17036755141ee605d5f2add
dagbouj hatem <dagboujhatem@gmail.com>
2021-12-02 11:39:29 +0100

    remove unused import

--- src/main/java/com/fivepoints/spring/Application.java
+++ src/main/java/com/fivepoints/spring/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "com.fivepoints.spring.*")
 @EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class,DataSourceAutoConfiguration.class})
